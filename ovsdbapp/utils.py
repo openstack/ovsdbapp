ad9b9fc64e88b88922a517a088cfa62a074b9b34
Miguel Duarte Barroso <mdbarroso@redhat.com>
2018-08-20 16:13:56 +0200

    Remove the oslo-utils dependency
    Change-Id: I34357d5a594330d07f5cad372859d98efb5d3fde
    (cherry picked from commit 5044279f5871797f8286183c35cc4d0db35b9ea8)

--- ovsdbapp/utils.py
+++ ovsdbapp/utils.py
@@ -11,6 +11,7 @@
 #    under the License.
 
 import netaddr
+import uuid
 
 # NOTE(twilson) Clearly these are silly, but they are good enough for now
 # I'm happy for someone to replace them with better parsing
@@ -41,3 +42,36 @@ def normalize_ip_port(ipport):
         if int(port) <= 0 or int(port) > 65535:
             raise netaddr.AddrFormatError("Invalid port")
         return "%s:%s" % (ip, port)
+
+
+def generate_uuid(dashed=True):
+    """Create a random uuid string.
+
+    :param dashed: Generate uuid with dashes or not
+    :type dashed: bool
+    :returns: string
+    """
+    if dashed:
+        return str(uuid.uuid4())
+    return uuid.uuid4().hex
+
+
+def _format_uuid_string(string):
+    return (string.replace('urn:', '')
+                  .replace('uuid:', '')
+                  .strip('{}')
+                  .replace('-', '')
+                  .lower())
+
+
+def is_uuid_like(val):
+    """Return validation of a value as a UUID.
+
+    :param val: Value to verify
+    :type val: string
+    :returns: bool
+    """
+    try:
+        return str(uuid.UUID(val)).replace('-', '') == _format_uuid_string(val)
+    except (TypeError, ValueError, AttributeError):
+        return False
