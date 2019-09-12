07b3b48fa42727a99e57f227b9b42cb3044e54f8
Lucas Alvares Gomes <lucasagomes@gmail.com>
2019-09-12 16:05:41 +0100

    Fix functional tests
    This patch is fixing the functional tests by forcing the use of the OVS
    2.11 branch instead of master (that's adequate for stein).
    
    The master branch has moved OVN out of it's repository as part of the
    commit https://patchwork.ozlabs.org/patch/1159033/ and the functional
    script in ovsdbapp no long works.
    
    Change-Id: Ia417da92c4c388bdabcd93b4f474233a7fac13dc
    Signed-off-by: Lucas Alvares Gomes <lucasagomes@gmail.com>

--- tools/setup-ovs.sh
+++ tools/setup-ovs.sh
@@ -7,5 +7,5 @@ if [ "$OVS_SRCDIR" -a ! -d "$OVS_SRCDIR" ]; then
     echo "Building OVS in $OVS_SRCDIR"
     mkdir -p $OVS_SRCDIR
     git clone git://github.com/openvswitch/ovs.git $OVS_SRCDIR
-    (cd $OVS_SRCDIR && ./boot.sh && PYTHON=/usr/bin/python ./configure && make -j$(($(nproc) + 1)))
+    (cd $OVS_SRCDIR && git checkout branch-2.11 && ./boot.sh && PYTHON=/usr/bin/python ./configure && make -j$(($(nproc) + 1)))
 fi
