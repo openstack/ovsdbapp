7d1f3180bd7900f4becc75de44d7fd2a8eba9692
Terry Wilson <twilson@redhat.com>
2022-03-21 21:23:55 +0000

    github no longer accepts git:// connections
    Change-Id: I30781ca6c72ec53c087e382e7138545e8483ff54

--- tools/setup-ovs.sh
+++ tools/setup-ovs.sh
@@ -14,12 +14,12 @@ function use_new_ovn_repository {
 if [ "$OVS_SRCDIR" -a ! -d "$OVS_SRCDIR" ]; then
     echo "Building OVS branch $OVS_BRANCH in $OVS_SRCDIR"
     mkdir -p $OVS_SRCDIR
-    git clone git://github.com/openvswitch/ovs.git $OVS_SRCDIR
+    git clone https://github.com/openvswitch/ovs.git $OVS_SRCDIR
     (cd $OVS_SRCDIR && git checkout $OVS_BRANCH && ./boot.sh && PYTHON=/usr/bin/python ./configure && make -j$(($(nproc) + 1)))
 fi
 if use_new_ovn_repository && [ "$OVN_SRCDIR" -a ! -d "$OVN_SRCDIR" ]; then
     echo "Building OVN branch $OVN_BRANCH in $OVN_SRCDIR"
     mkdir -p $OVN_SRCDIR
-    git clone git://github.com/ovn-org/ovn.git $OVN_SRCDIR
+    git clone https://github.com/ovn-org/ovn.git $OVN_SRCDIR
     (cd $OVN_SRCDIR && git checkout $OVN_BRANCH && ./boot.sh && PYTHON=/usr/bin/python ./configure --with-ovs-source=$OVS_SRCDIR && make -j$(($(nproc) + 1)))
 fi
