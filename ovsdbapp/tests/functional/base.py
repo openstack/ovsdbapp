9c1790796d8094d386257ebd2909186a9d63352c
Terry Wilson <twilson@redhat.com>
2025-07-10 17:49:08 +0000

    Rework venv to support arbitrary schemas
    Change-Id: Iad56cad573ba1a9489526271d513cd3861760690
    Signed-off-by: Terry Wilson <twilson@redhat.com>

--- ovsdbapp/tests/functional/base.py
+++ ovsdbapp/tests/functional/base.py
@@ -10,9 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import atexit
 import os
-import tempfile
 
 from ovsdbapp.backend.ovs_idl import connection
 from ovsdbapp import constants
@@ -20,7 +18,39 @@ from ovsdbapp.tests import base
 from ovsdbapp import venv
 
 
-class FunctionalTestCase(base.TestCase):
+class VenvPerClassFunctionalTestCase(base.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.virtualenv = venv.VenvFixture(
+            remove=not bool(os.getenv("KEEP_VENV")))
+        cls.virtualenv.setUp()
+        cls.ovsvenvlog = None
+        if os.getenv('KEEP_VENV') and os.getenv('VIRTUAL_ENV'):
+            cls.ovsvenvlog = open(
+                os.path.join(os.getenv('VIRTUAL_ENV'),
+                             'ovsvenv.%s' % os.getpid()), 'a+')
+            cls.ovsvenvlog.write("%s\n" % cls.ovsvenv.venv)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.ovsvenvlog:
+            cls.ovsvenvlog.close()
+        cls.virtualenv.cleanUp()
+        super().tearDownClass()
+
+    @classmethod
+    def venv_log(cls, val):
+        if cls.ovsvenvlog:
+            cls.ovsvenvlog.write("%s\n" % val)
+
+    def setUp(self):
+        super().setUp()
+        self.venv_log(self.id())
+
+
+class FunctionalTestCase(VenvPerClassFunctionalTestCase):
     _connections = None
     fixture_class = venv.OvsOvnVenvFixture
 
@@ -28,28 +58,19 @@ class FunctionalTestCase(base.TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.ovsvenv = cls.fixture_class(
-            tempfile.mkdtemp(),
+            cls.virtualenv,
             ovsdir=os.getenv('OVS_SRCDIR'),
-            ovndir=os.getenv('OVN_SRCDIR'),
-            remove=not bool(os.getenv('KEEP_VENV')))
-        atexit.register(cls.ovsvenv.cleanUp)
+            ovndir=os.getenv('OVN_SRCDIR'))
         cls.ovsvenv.setUp()
         cls.schema_map = {'Open_vSwitch': cls.ovsvenv.ovs_connection,
                           'OVN_Northbound': cls.ovsvenv.ovnnb_connection,
                           'OVN_Southbound': cls.ovsvenv.ovnsb_connection,
                           }
-        cls.ovsvenvlog = None
-        if os.getenv('KEEP_VENV') and os.getenv('VIRTUAL_ENV'):
-            cls.ovsvenvlog = open(
-                os.path.join(os.getenv('VIRTUAL_ENV'),
-                             'ovsvenv.%s' % os.getpid()), 'a+')
-            atexit.register(cls.ovsvenvlog.close)
-            cls.ovsvenvlog.write("%s\n" % cls.ovsvenv.venv)
 
     @classmethod
-    def venv_log(cls, val):
-        if cls.ovsvenvlog:
-            cls.ovsvenvlog.write("%s\n" % val)
+    def tearDownClass(cls):
+        cls.ovsvenv.cleanUp()
+        super().tearDownClass()
 
     @property
     def connection(self):
@@ -71,6 +92,5 @@ class FunctionalTestCase(base.TestCase):
         return connection.Connection(idl, constants.DEFAULT_TIMEOUT)
 
     def setUp(self):
-        super(FunctionalTestCase, self).setUp()
-        self.venv_log(self.id())
+        super().setUp()
         self.set_connection()
